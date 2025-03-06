073530ddfd2a0c7f9096643295847cd05c2f6026
srujaniyengar <srujanparthasarathyiyengar@gmail.com>
2025-03-06 13:59:54 +0530

    update: main

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
-package unqueassignment
+package main
+
+import (
+	"UnQue/configs"
+	"UnQue/routes"
+	"log"
+)
+
+func main() {
+	configs.ConnectDB()
+
+	router := routes.SetupRoutes()
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal("Failed to start server: ", err)
+	}
+}
