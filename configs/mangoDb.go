61054ec6b96f9a1cbf98a93314063319a8323330
srujaniyengar <srujanparthasarathyiyengar@gmail.com>
2025-03-06 13:34:11 +0530

    update: mangoDb.go with utils

--- configs/mangoDb.go
+++ configs/mangoDb.go
@@ -1,1 +1,31 @@
 package configs
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var DB *mongo.Database
+
+func ConnectDB() *mongo.Database {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+
+	if err = client.Ping(ctx, nil); err != nil {
+		log.Fatal("Error pinging:", err)
+	}
+
+	DB = client.Database("college_appointment")
+	return DB
+}
