2a05919c526a3399ef751beb559b2214042aed1c
srujaniyengar <srujanparthasarathyiyengar@gmail.com>
2025-03-06 13:58:38 +0530

    update: routes with controls

--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,25 @@
 package routes
+
+import (
+	"UnQue/control"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoutes() *gin.Engine {
+	router := gin.Default()
+
+	router.POST("/login", control.Login)
+
+	router.POST("/availability", control.SetAvailability)
+
+	router.GET("/availability", control.GetAvailability)
+
+	router.POST("/appointments", control.BookAppointment)
+
+	router.DELETE("/appointments/:id", control.CancelAppointment)
+
+	router.GET("/appointments", control.GetAppointments)
+
+	return router
+}
